d2e26c13dff481d31290ef5d6427666c9ab6759e
Pierre Bourgeois <pbcomweb@gmail.com>
2019-12-30 18:00:04 -0500

    maquette vignette sm, md et lg

--- pages/vignettes.vue
+++ pages/vignettes.vue
@@ -5,27 +5,47 @@
             <ul>
                 <li>
                     <a href="">
-                        <div class="inner">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, iusto?</div>
+                        <div class="inner">
+                            <img src="../static/img/domicile_bien_argent.png" aria-hidden="true" alt="Image décorative">
+                            <h2>Domicile, Biens, Argent</h2>
+                            <p>Partage des biens, Pension alimentaire pour ex-conjoint, Aides gouvernementales</p>
+                        </div>
                     </a>
                 </li>
                 <li>
                     <a href="">
-                        <div class="inner">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, iusto?</div>
+                        <div class="inner"> 
+                            <img src="../static/img/enfant.png" aria-hidden="true" alt="Image décorative">
+                            <h2>Enfant</h2>
+                            <p>Annonce de la séparation, Pension alimentaire pour enfants, Garde, Vie quotidienne et éducation</p>
+                        </div>
                     </a>
                 </li>
                 <li>
                     <a href="">
-                        <div class="inner">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, iusto?</div>
+                        <div class="inner"> 
+                            <img src="../static/img/adapter_separation.png" aria-hidden="true" alt="Image décorative">
+                            <h2>S'adapter à la séparation</h2>
+                            <p>S’organiser, Communiquer avec son ex, Émotions</p>
+                        </div>
+                    </a>
+                </li>           
+                <li>
+                    <a href="">
+                        <div class="inner"> 
+                            <img src="../static/img/mediation.png" aria-hidden="true" alt="Image décorative">
+                            <h2>Médiation</h2>
+                            <p>Déroulement, Préparation, Trouver un médiateur</p>
+                        </div>
                     </a>
                 </li>
                 <li>
                     <a href="">
-                        <div class="inner">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, iusto?</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <div class="inner">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, iusto?</div>
+                        <div class="inner"> 
+                            <img src="../static/img/tribunal.png" aria-hidden="true" alt="Image décorative">
+                            <h2>Tribunal</h2>
+                            <p>Procédures, Préparation, Déroulement de l’audience</p>
+                        </div>
                     </a>
                 </li>
             </ul>
